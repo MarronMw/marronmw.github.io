65b3f370a348352852fcdba51286133116dc7f18
joshypeace <joshuachilapondwa2@gmail.com>
2024-07-26 09:45:38 +0200

    email sending finallized

--- letsgo/Js/main.js
+++ letsgo/Js/main.js
@@ -32,28 +32,45 @@ var swiper = new Swiper(".mySwiper", {
 
 // on scroll gestures
 
-window.addEventListener('scroll', reveal);
+window.addEventListener("scroll", reveal);
 
-function reveal(){
-  var reveals = document.querySelectorAll('.reveal');
-  
-  for (var i = 0; i < reveals.length; i++){
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
 
-      var window_height = window.innerHeight;
-      var reveal_top = reveals[i].getBoundingClientRect().top;
-      var reveal_point  = 150;
+  for (var i = 0; i < reveals.length; i++) {
+    var window_height = window.innerHeight;
+    var reveal_top = reveals[i].getBoundingClientRect().top;
+    var reveal_point = 150;
 
-      if(reveal_top < window_height - reveal_point){
-        reveals[i].classList.add('active');
-      }
-
-      else{
-        reveals[i].classList.remove('active');
-      }
-
-
+    if (reveal_top < window_height - reveal_point) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
   }
-
-
-
 }
+
+// dynamiclink on send
+document.addEventListener("DOMContentLoaded", (event) => {
+  let nameInput = document.getElementById("name");
+  let emailInput = document.getElementById("email");
+  let messageInput = document.getElementById("message");
+  let sendLink = document.getElementById("SEND");
+  let generateButton = document.getElementById("SEND-BTN");
+
+  generateButton.addEventListener("click", () => {
+    // Retrieve the values from the input fields
+    let nameValue = encodeURIComponent(nameInput.value);
+    let emailValue = encodeURIComponent(emailInput.value);
+    let messageValue = encodeURIComponent(messageInput.value);
+
+    // Set the href attribute of the link with the dynamic data
+    sendLink.setAttribute(
+      "href",
+      `mailto:info@letsgotravelandtours.com?cc=letsgotravelandtours299@gmail.com&subject=Hello%20Let's%20Go%20Travels%20and%20Tours&body=Name: ${nameValue}%0AEmail: ${emailValue}%0AMessage: ${messageValue}`
+    );
+
+    // Make the link visible
+    sendLink.style.display = "inline";
+  });
+});
