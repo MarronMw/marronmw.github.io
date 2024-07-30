98ffb047af3fea06bc15aec78a4366df3ccfb130
Timothy <msiskazikani@gmail.com>
2024-07-30 13:54:37 +0200

    newer

--- portfolio/js/script.js
+++
@@ -1,7 +0,0 @@
-const navbarToggler = document.getElementById('navbar-toggler');
-        const navLinks = document.getElementById('nav-links');
-
-        navbarToggler.addEventListener('click', () => {
-            navbarToggler.classList.toggle('active');
-            navLinks.classList.toggle('active');
-        });
